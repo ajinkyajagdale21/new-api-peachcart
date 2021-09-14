76e8ef616c9feaa233f8faae6fc229e28b8e0802
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-09-14 11:36:57 +0530

    update: solved login bug

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 8000
-const products =require("./routes/product.route")
+const products = require("./routes/product.route")
 const cart = require('./routes/cart.route')
 const wishlist = require('./routes/wishlist.route')
 const auth = require('./routes/auth.route')
-const {initialConnection} = require('./DBconnection/db.connect')
-const {addUserToDb} = require('./models/users.model')
+const { initialConnection } = require('./DBconnection/db.connect')
+const { addUserToDb } = require('./models/users.model')
+require("dotenv").config();
 //const {Product,addProductToDB} = require('./models/product.model')
 
 
@@ -18,20 +19,20 @@ initialConnection();
 //addProductToDB();
 app.use(cors());
 app.use(bodyParser.json())
-app.use('/products',products)
-app.use('/cart',cart)
-app.use('/wishlist',wishlist)
-app.use('/auth',auth)
+app.use('/products', products)
+app.use('/cart', cart)
+app.use('/wishlist', wishlist)
+app.use('/auth', auth)
 
-app.get('/' , (req , res) => {
+app.get('/', (req, res) => {
     res.send('hello express')
 })
-app.use((req , res) => {
-    res.status(404).json({success : false  , message : 'page not found'})
+app.use((req, res) => {
+    res.status(404).json({ success: false, message: 'page not found' })
 })
 
-app.use((err , req ,res , next) => {
+app.use((err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).json({success : false , message : 'something went wrong' , error : err.message})
+    res.status(500).json({ success: false, message: 'something went wrong', error: err.message })
 })
 app.listen(PORT, () => console.log('server started at port', PORT))
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,4 @@
-const express= require('express');
+const express = require('express');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { User } = require('../models/users.model');
@@ -7,50 +7,49 @@ const { Wishlist } = require('../models/wishlist.model');
 const router = express.Router();
 
 router.route('/signup')
-    .post(async(req,res)=>{
-        const user= req.body;
-        try{
-            const checkUser= await User.findOne({email:user.email})
-            if(checkUser){
-                return res.status(403).json({success:false,message:"User Already exists, Login to continue"})
+    .post(async (req, res) => {
+        const user = req.body;
+        try {
+            const checkUser = await User.findOne({ email: user.email })
+            if (checkUser) {
+                return res.status(403).json({ success: false, message: "User Already exists, Login to continue" })
             }
             const NewUser = new User(user)
             const salt = await bcrypt.genSalt(10);
-            NewUser.password= await bcrypt.hash(NewUser.password,salt)
+            NewUser.password = await bcrypt.hash(NewUser.password, salt)
             await NewUser.save();
-            const NewCart = new Cart({uid: NewUser._id, items:[]});
+            const NewCart = new Cart({ uid: NewUser._id, items: [] });
             await NewCart.save();
-            const NewWishlist = new Wishlist({uid: NewUser._id , items:[]});
+            const NewWishlist = new Wishlist({ uid: NewUser._id, items: [] });
             await NewWishlist.save();
-            res.status(201).json({success:true,user:NewUser})
+            res.status(201).json({ success: true, user: NewUser })
         }
-        catch(error){
-            res.status(400).json({success:false,message:error.message})
+        catch (error) {
+            res.status(400).json({ success: false, message: error.message })
         }
     })
 
-    router.route('/login')
-    .post(async(req,res)=>{
-        try{
-        const {email,password} = req.body;
-        const user= await User.findOne({email})
-        if(user){
-            const validPassword = await bcrypt.compare(password,user.password)
-            if(validPassword){
-                 const token = jwt.sign({userId: _id},process.env.KEY,{expiresIn:"24h"})
-                 res.status(200).json({ name: user.firstName, token, userId: user._id });
+router.route('/login')
+    .post(async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({ email })
+            if (user) {
+                const validPassword = await bcrypt.compare(password, user.password)
+                if (validPassword) {
+                    const token = jwt.sign({ userId: user._id }, process.env.KEY, { expiresIn: "24h" })
+                    res.status(200).json({ name: user.firstName, token, userId: user._id });
+                }
+                else {
+                    return res.status(401).json({ success: false, message: "incorrect password" })
+                }
             }
-            else{
-            return res.status(401).json({success: false, message: "incorrect password"})     
+            else {
+                return res.status(401).json({ success: false, message: "User not found ! Please Sign Up" })
+            }
         }
-    }
-      else
-        { 
-            return res.status(401).json({success: false, message: "User not found ! Please Sign Up"})
+        catch (error) {
+            res.status(400).json({ success: false, message: error.message });
         }
-    }
-    catch(error){
-        res.status(400).json({ success: false, message: error.message });
-    }
     })
-    module.exports = router;
+module.exports = router;
