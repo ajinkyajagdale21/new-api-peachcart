e3896e98db4dd3ffda605f5067169aad66ed8c33
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2021-09-15 12:35:09 +0530

    update: secured database api

--- DBconnection/db.connect.js
+++ DBconnection/db.connect.js
@@ -1,18 +1,18 @@
-const mongoose= require('mongoose')
+const mongoose = require('mongoose')
 
-const initialConnection=async()=>{
-    try{
-        const connection = await mongoose.connect('mongodb+srv://ajinkyajagdale:ajinkya@21@peachcart.46ya6.mongodb.net/peachcart', {
-            useUnifiedTopology : true,
-            useNewUrlParser : true
+const initialConnection = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
         })
-        if(connection){
+        if (connection) {
             console.log("connection Successful")
         }
     }
-    catch(error){
-       console.log("connection failed ", error)
+    catch (error) {
+        console.log("connection failed ", error)
     }
 }
 
-module.exports= {initialConnection}
+module.exports = { initialConnection }
